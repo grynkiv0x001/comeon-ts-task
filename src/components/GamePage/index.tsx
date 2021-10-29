ebdf8b4a952605121a708249b782ac5cc3886760
Bohdan Hrynkiv <grynkiv0x001@gmail.com>
2021-10-29 17:06:09 +0300

    Fullscreen switcher added

--- src/components/GamePage/index.tsx
+++ src/components/GamePage/index.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import { Button } from "@mui/material";
 
 import { ROUTES } from "../../core/constants/routes";
 
 import './GamePage.scss';
-import { Button } from "@mui/material";
 
 const windowLocal: any = window;
 
 export const GamePage = () => {
   const history = useHistory();
   const [cookie] = useCookies(['user']);
+  const gameLaunch = useRef<any>(null);
   const { name }: { name: string } = useParams();
+  let fullscreen = false;
 
   useEffect(() => {
     if (!cookie.user) {
@@ -26,15 +28,39 @@ export const GamePage = () => {
     }
   }, [name, windowLocal.comeon]);
 
-  const handleClick = () => {
+  const handleClickBack = () => {
     history.push(ROUTES.HOME);
+  };
+
+  const handleClickFullscreen = () => {
+    const current = gameLaunch?.current;
+    fullscreen = !fullscreen;
+
+    if (fullscreen) {
+      current.className = 'game-launch--fullscreen';
+    } else {
+      current.className = '';
+    }
   };
 
   return (
     <>
-      <Button className="button-back" onClick={handleClick} variant="contained">Go back</Button>
+      <Button
+        className="game-button game-button--back"
+        variant="contained"
+        onClick={handleClickBack}
+      >
+        Go back
+      </Button>
+      <Button
+        className="game-button game-button--fullscreen"
+        variant="contained"
+        onClick={handleClickFullscreen}
+      >
+        Fullscreen
+      </Button>
       <div className="game-place">
-        <div id="game-launch" />
+        <div id="game-launch" ref={gameLaunch} />
       </div>
     </>
   );
